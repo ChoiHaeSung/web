d558498c5eb910a13aebc3191e68d7a092ab7c27
USER\User <dlfl1999@naver.com>
2024-04-11 11:44:32 +0900

    으뜸 파이썬

---
+++ 파이썬/1장/test0101.py
@@ -0,0 +1,35 @@
+# 1-1 hello python 출력하기
+print('Hello Python')
+
+# 1-2 hello Python 3번 출력하기
+print('Hello Python')
+print('Hello Python')
+print('Hello Python')
+
+# 대화창 실습 : 대화창에서문자열과 숫자 출력하기
+print('Hello world!')
+print('Hello','world!')
+print('Hello'+'world!')
+print(100)
+print(2+4)
+
+# 연습문제
+print(100)
+print(100 + 200)
+print('100 + 200')
+print(100, 200)
+print('100', '200')
+print('100''200')
+print('Hello Python!')
+print('Hello','Python', '!')
+print('Hello'+'Python'+'!')
+print('Hello''Python''!')
+print('********')
+print('+' * 10)
+
+
+# 1.4 어떤 오류를 출력하는가?
+print('Hello Python!')
+print("Hello Python!")
+# print(Hello Python!)
+print(str(100)+'200')
---
+++ 파이썬/test1.py
@@ -0,0 +1,13 @@
+# LAB 3-3 :if 조건문의 응용
+# 1번 문제
+
+game_score = input('게임점수를 입력하시오 : ')
+print('game score = ',game_score)
+
+if game_score >= 1000 :
+    print('고수입니다')
+else :
+    print('입문자입니다')
+
+
+
