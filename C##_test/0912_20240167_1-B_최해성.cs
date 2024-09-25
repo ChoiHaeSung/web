b4c6f65ea2041f09c8402fcf0b4ccf816cdd408d
DESKTOP-IVP5V38\D503 <dlfl1999@naver.com>
2024-09-25 16:42:49 +0900

    2학기 C#

---
+++ C##_test/0912_20240167_1-B_최해성.cs
@@ -0,0 +1,146 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// 75P
+Console.WriteLine("대한\t민국.");
+Console.WriteLine("용기는 \" 진실\'에서 우러나온다.");
+Console.WriteLine("오늘보다\n내일은 희망차다.");
+
+
+
+// 77P
+Console.WriteLine("대한민국."[0]);
+Console.WriteLine("대한민국."[1]);
+Console.WriteLine("대한민국."[2]);
+Console.WriteLine("대한민국."[3]);
+
+// 81P
+Console.Write("!false의 논리 연산 결과 : ");
+Console.WriteLine(!false);
+Console.Write("true || false의 논리 연산 결과 : ");
+Console.WriteLine(true || false);
+Console.Write("true && false의 논리 연산 결과 : ");
+Console.WriteLine(true && false);
+
+// 92P
+int a, b, hap, cha, gob, na, mo;
+a = 10;
+b = 3;
+
+hap = a + b;
+cha = a - b;
+gob = a * b;
+na = a / b;
+
+Console.WriteLine("a=10, b= 3일 때, 사칙 연산결과");
+Console.WriteLine("a + b = " + hap);
+Console.WriteLine("a - b = " + cha);
+Console.WriteLine("a * b = " + gob);
+Console.WriteLine("a / b = " + na);
+
+// 95p
+float c, d, result1;
+double result2;
+c = 2;
+d = 3;
+result1 = c / d;
+result2 = c / d;
+
+Console.WriteLine("float = " + result1);
+Console.WriteLine("double = " + result2);
+
+// 96p
+float e,f, result3;
+e = 2;
+f = 3;
+result3 =  e / f;
+
+double g, h, result4;
+e = 2;
+f = 3;
+result4 = e / f;
+
+Console.WriteLine("float = " + result3);
+Console.WriteLine("double = " + result4);
+
+// 98p
+decimal j, k;
+j = 0.12345678901234567890123m; // 숫자 뒤에 m을 반드시 첨부
+k = 3.141592653589793238462643383279502884197m;
+
+Console.WriteLine("decimal = " + j);
+Console.WriteLine("decimal = " + k);
+
+// 100p
+char l, n;
+l = 'A';
+n = 'B';
+
+Console.Write("알파벳 : " + l);
+Console.WriteLine("-> 아스키코드값 : " + (int)n);
+
+// 102p
+string m = "집에 가고싶다";
+
+Console.WriteLine(m);
+
+// 103p
+string o = "prongraming";
+
+Console.WriteLine(o + "!");
+Console.WriteLine(o[1]);
+Console.WriteLine(o[4]);
+
+// 104p
+string sz = "Space Zone";
+Console.WriteLine("대문자 변환 " + sz.ToUpper());
+Console.WriteLine("소문자 변환 " + sz.ToLower());
+// 105p
+string tt = sz.Trim();
+Console.WriteLine("모든 공백 제거 |" + sz.Trim() + "|");
+Console.WriteLine("앞의 공백 제거 |" + sz.TrimStart() + "|");
+Console.WriteLine("뒤의 공백 제거 |" + sz.TrimEnd() + "|");
+
+// 107p
+bool q = (37 == 99);
+Console.WriteLine("!(37 == 99) : " + c);
+
+// 109p
+Console.WriteLine("int 형식 \t : " + sizeof(int) + " 바이트");
+
+// 110p
+int p = 123;
+long r = 1234567890123456;
+
+// 변수에 대한 자료형 검사
+Console.WriteLine(p.GetType());     // int
+Console.WriteLine(r.GetType());     // long
+
+// 상수에 대한 자료형 검사
+Console.WriteLine(38.277f.GetType());   // float
+Console.WriteLine(38.277.GetType());    // Double
+Console.WriteLine(38.277m.GetType());   // decimal
+Console.WriteLine("스페이스".GetType());// string
+Console.WriteLine("A".GetType());       // char
+
+// 112p
+var s = 88.99;
+var t = 'A';
+var u = "space";
+
+Console.WriteLine(s.GetType());
+Console.WriteLine(t.GetType());
+Console.WriteLine(u.GetType());
+
+// 113p
+int v, w;
+float result5;
+
+v = 5;
+w = 2;
+
+result5 = v / w;
+Console.WriteLine("강제 자료형 변환 전 :" + result5);
+
+result5 = (float)v / w;
+Console.WriteLine("강제 자료형 변환 전 : " + result5);
---
+++ C##_test/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,285 @@
+﻿/*
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// 117p
+int count = 100;
+
+Console.WriteLine("주어진 데이터값이 count = 100 일 때");
+count += 5; // count = count + 5
+Console.WriteLine("count += 5 " + count);
+
+count -= 3; // count = count - 5
+Console.WriteLine("count -= 3 " + count);
+
+count *= 2; // count = count * 2
+Console.WriteLine("count * =2 " + count);
+
+count /= 10; // count = count / 10
+Console.WriteLine("count /= 10 " + count);
+
+// 118p
+string fruti = "과일 -> ";
+
+Console.WriteLine("주어진 문자열 : " +  fruti);
+
+fruti += "사과";
+Console.WriteLine("주어진 문자열 : " +  fruti);
+
+fruti += "바나나";
+Console.WriteLine("주어진 문자열 : " +  fruti);
+
+//119
+int b, c;
+
+b = 10;
+c = ++b;
+Console.Write("b = 10, c = ++b 일 때 : ");
+Console.WriteLine("b = " + b + ", c = " + c);
+
+b = 10; //누적값을 배제하기 위한 변수의 값 초기화 선언
+c = b++;
+Console.Write("b = 10, c = b++ 일 때 : ");
+Console.WriteLine("b = " + b + ", c = " + c);
+
+b = 10; //누적값을 배제하기 위한 변수의 값 초기화 선언
+c = --b;
+Console.Write("b = 10, c = --b 일 때 : ");
+Console.WriteLine("b = " + b + ", c = " + c);
+
+b = 10; //누적값을 배제하기 위한 변수의 값 초기화 선언
+c = b--;
+Console.Write("b = 10, c = b-- 일 때 : ");
+Console.WriteLine("b = " + b + ", c = " + c);
+
+
+//122p
+int d;
+
+Console.Write("1. 정수 입력 :");
+d = int.Parse(Console.ReadLine());
+
+Console.WriteLine("2. 입력한 정수값 출력 : " + d);
+
+// 123p
+float e;
+
+Console.Write("1. 실수 입력 : ");
+e = float.Parse(Console.ReadLine());
+
+Console.WriteLine("2. 입력한 실수값 출력 : " + e);
+
+
+
+
+//125p
+int h;
+char cparse;
+
+Console.Write("1. 문자입력 : ");
+h = Console.Read();
+
+Console.WriteLine("2. 문자의 아스키코드값이 출력 : " + h);
+cparse = Convert.ToChar(h);
+
+Console.WriteLine("3. 문자 변환 메서드를 적용하여 출력 : " + cparse);
+
+//126p
+Console.ReadLine();
+
+string g;
+
+Console.Write("1. 문자열 입력 : ");
+g = Console.ReadLine();
+
+Console.WriteLine("2. 입력한 문자열 출력 : " + g);
+
+
+//146p
+int a, choice;
+
+Console.Write("정수 입력 : ");
+a = int.Parse(Console.ReadLine());
+choice = a % 2;
+
+if (choice == 0)
+{
+    Console.WriteLine("짝수");
+}
+else
+{
+    Console.WriteLine("홀수");
+}
+
+//147p
+int c;
+char cparse;
+
+Console.Write("문자 입력:");
+c = Console.Read();
+cparse = Convert.ToChar(c);
+
+if (cparse >= 'A' && cparse <= 'Z')
+{
+    Console.WriteLine(" > 입력한 알파벳 : " + cparse);
+    Console.WriteLine(" > 판별 결과 : 대문자 ");
+}
+else
+{
+    Console.WriteLine(" > 입력한 알파벳 : " + cparse);
+    Console.WriteLine(" > 판별 결과 : 소문자 ");
+}
+
+
+//149p
+int d;
+char chocolet;
+
+Console.WriteLine("법학과 : A 또는 a ");
+Console.WriteLine("행정학과 : S 또는 s ");
+Console.WriteLine("소비자학과 : D 또는 d ");
+Console.WriteLine("데이터사이언스학과 : F 또는 f");
+Console.WriteLine("그 이외의 알파벳 : 에러 메시지 출력 ");
+Console.WriteLine();
+
+Console.Write("알파벳 입력 : ");
+d = Console.Read();
+chocolet = Convert.ToChar(d);
+
+Console.Write("선택학과 : ");
+
+if (chocolet == 'A' || chocolet == 'a')
+{
+    Console.WriteLine("법학과 ");
+}
+else if (chocolet == 'S' || chocolet == 'S') 
+{
+    Console.WriteLine("행정학과 ");
+}
+
+
+//151
+int score;
+
+Console.Write("점수 입력: ");
+score = int.Parse(Console.ReadLine());
+
+if (score >= 90)
+{
+    Console.WriteLine(" > 입력한 점수 : " + score);
+    Console.WriteLine(" > 등급 : A 학점 ");
+}
+if (score >= 80)
+{
+    Console.WriteLine(" > 입력한 점수 : " + score);
+    Console.WriteLine(" > 등급 : B 학점 ");
+}
+if (score >= 70)
+{
+    Console.WriteLine(" > 입력한 점수 : " + score);
+    Console.WriteLine(" > 등급 : C 학점 ");
+}
+if (score >= 60)
+{
+    Console.WriteLine(" > 입력한 점수 : " + score);
+    Console.WriteLine(" > 등급 : D 학점 ");
+}
+else
+{
+    Console.WriteLine(" > 입력한 점수 : " + score);
+    Console.WriteLine(" > 등급 : F 학점 ");
+}
+
+
+//159p
+int score, mok;
+char grade;
+
+Console.Write("점수 입력 : ");
+score = int.Parse(Console.ReadLine());
+mok = score / 10;
+
+if (score > 100 || score < 0)
+{
+    Console.WriteLine(" > 점수의 허용 공간이 아닙니다. ");
+    Console.WriteLine(" > 프로그램 종료! ");
+}
+else
+{
+    switch (mok)
+    {
+        case 10:
+        case 9:
+            grade = 'A';
+            break;
+        case 8:
+            grade = 'B';
+            break;
+        case 7:
+            grade = 'C';
+            break;
+        case 6:
+            grade = 'D';
+            break;
+        default:
+            grade = 'F';
+            break;
+    }
+    Console.WriteLine(" > 입력한 점수 : " + score );
+    Console.WriteLine(" > 등급 : " + grade + " 학점 ");
+}
+*/
+/*
+//quit 1
+int age;
+char cparse;
+
+Console.Write("나이 입력");
+age = int.Parse(Console.ReadLine());
+
+if (age >= 18 && age < 99)
+
+    Console.WriteLine(" > 나이 : 청소년 관람 불가");
+}
+if (age >= 15 && age < 18)
+{
+    Console.WriteLine(" > 나이 : 15세 이상 관람가");
+}
+else 
+{
+    Console.WriteLine(" > 나이 : 전체 관람가");
+}
+*/
+// quit 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
