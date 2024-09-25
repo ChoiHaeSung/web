7db8196c3f4774f9ca44bec5a2e83361b4e9e3e8
DESKTOP-IVP5V38\D503 <dlfl1999@naver.com>
2024-09-25 16:44:26 +0900

    2학기 자바 수업

---
+++ JAVA_test/2. Variable/Hello.java
@@ -0,0 +1,25 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int a = 4;
+		int b = 2;
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		
+//		System.out.println(4+2);
+//		System.out.println(4-2);
+//		System.out.println(4*2);
+//		System.out.println(4/2);
+//
+//		System.out.println(6+3);
+//		System.out.println(6-3);
+//		System.out.println(6*3);
+//		System.out.println(6/3);
+	}
+}
+
+
